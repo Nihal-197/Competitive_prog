86ecbc7951b1f3cc318350b7f6a1e17d81bff151
Nihal Singh <singhnihal1111@gmail.com>
2019-07-01 13:19:22 +0530

    Dang

---
+++ ATM.py
@@ -0,0 +1,7 @@
+x,balance=map(float,input().split())
+if (x%5==0) and (balance>=x+0.5):
+    balance=balance -(x+0.5)
+    print(balance)
+else :
+    print(balance)
+   
---
+++ CHRL4.py
@@ -0,0 +1,20 @@
+# %%
+n,k=map(int,input().split())
+arr = list(map(int,input().split()))
+
+product=1
+for i in range(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
